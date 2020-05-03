52bc0bbff4fe367dd5b55041fa2fa7b1e4cc8ac6
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-05-03 12:27:10 -0300

    atomic components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import MainTemplate from './templates/main/'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainTemplate></MainTemplate>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/templates/main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainTemplate() {
+    return (
+        <div>
+            asdasd
+        </div>
+    );
+}
+
+export default MainTemplate;
